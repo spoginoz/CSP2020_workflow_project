4f9538d90d683df3bfe93321b95faf4f13c52ff3
Shannon Monahan <smonahan@stanford.edu>
2020-02-22 15:26:31 -0800

    new R code file

---
+++ animal_data_fun.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+
+library(readr)
+zooAnimal_medianLifeExpectancy_Jul2018_ <- read_csv("zooAnimal_medianLifeExpectancy_Jul2018 .csv")
+View(zooAnimal_medianLifeExpectancy_Jul2018_)
+
