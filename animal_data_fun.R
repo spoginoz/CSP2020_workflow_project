5172943fbcb4d4b96bd4410948c35454edbd420c
Shannon Monahan <smonahan@stanford.edu>
2020-02-22 15:35:50 -0800

    trying to commit yet again.

--- animal_data_fun.R
+++ animal_data_fun.R
@@ -5,10 +5,10 @@ library(readr)
 zooAnimal_medianLifeExpectancy_Jul2018_ <- read_csv("zooAnimal_medianLifeExpectancy_Jul2018 .csv")
 View(zooAnimal_medianLifeExpectancy_Jul2018_)
 
-colnames(zooAnimal_medianLifeExpectancy_Jul2018_)
-
 sub_zoo <- zooAnimal_medianLifeExpectancy_Jul2018_ %>%
   select("TaxonClass")
 
 view (sub_zoo)
 ##ADDING NOTES
+##adding more notes....
+##lalallala
